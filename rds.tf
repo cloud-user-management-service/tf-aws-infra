7b01673d7d73e35bc9fa449b971b13df7ff38df0
lllinginging <llling10099@gmail.com>
2024-10-25 16:55:05 -0700

    terraform fmt

--- rds.tf
+++ rds.tf
@@ -3,12 +3,12 @@ resource "aws_db_instance" "mysql_instance" {
   allocated_storage = 20
   engine            = "mysql"
   engine_version    = "8.0"
-  instance_class    = "db.t3.micro" 
-  multi_az          = false         
+  instance_class    = "db.t3.micro"
+  multi_az          = false
   //DB instance identifier
   identifier = "csye6225"
   username   = "csye6225"
-  password   = var.db_password 
+  password   = var.db_password
   //subnet group
   db_subnet_group_name = aws_db_subnet_group.mysql_subnet_group.name
   publicly_accessible  = false # Do not expose to the internet
