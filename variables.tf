e3abf78534fa794f27a32a59beb5775b2841bbd1
lllinginging <llling10099@gmail.com>
2024-11-08 21:56:54 -0800

    fix issue of a06: the A record is either created or updated with the value set to the EC2 instance's public IP 3

--- variables.tf
+++ variables.tf
@@ -10,10 +10,28 @@ variable "region" {
   default     = "us-west-2"
 }
 
-variable "subnet_cidr_block" {
+variable "public_subnet1_cidr_block" {
   type        = string
   description = "The CIDR block for the subnets"
   default     = "192.12.1.0/24"
+}
+
+variable "public_subnet2_cidr_block" {
+  type        = string
+  description = "The CIDR block for the subnets"
+  default     = "192.12.2.0/24"
+}
+
+variable "private_subnet1_cidr_block" {
+  type        = string
+  description = "The CIDR block for the private subnets"
+  default     = "192.12.3.0/24"
+}
+
+variable "private_subnet2_cidr_block" {
+  type        = string
+  description = "The CIDR block for the private subnets"
+  default     = "192.12.4.0/24"
 }
 
 variable "ami_id" {
@@ -46,6 +64,24 @@ variable "allow_ports" {
   default     = [22, 80, 443, 8081]
 }
 
+variable "ingress_port_http" {
+  description = "The port for HTTP traffic"
+  type        = number
+  default     = 80
+}
+
+variable "ingress_port_https" {
+  description = "The port for HTTPS traffic"
+  type        = number
+  default     = 443
+}
+
+variable "ssh_port" {
+  description = "The port for SSH traffic"
+  type        = number
+  default     = 22
+}
+
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
@@ -55,13 +91,13 @@ variable "availability_zones" {
 variable "private_subnet_cidr_block_a" {
   type        = string
   description = "The CIDR block for the private subnets"
-  default     = "192.12.2.0/24"
+  default     = "192.12.5.0/24"
 }
 
 variable "private_subnet_cidr_block_b" {
   type        = string
   description = "The CIDR block for the private subnets"
-  default     = "192.12.3.0/24"
+  default     = "192.12.6.0/24"
 }
 
 variable "db_password" {
@@ -71,11 +107,6 @@ variable "db_password" {
   default = "CSYE6225password"
 }
 
-variable "dev_domain_zone_id" {
-  type        = string
-  description = "The zone ID for the S3 bucket"
-  default     = "Z00435553RLQ8XGFYJJS3"
-}
 
 variable "db_identifier" {
   type        = string
@@ -96,3 +127,150 @@ variable "db_name" {
 }
 
 
+variable "domain_zone_id" {
+  type        = string
+  description = "The zone ID for the Route53"
+  default     = "Z00435553RLQ8XGFYJJS3"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "The domain name for the Route53 record"
+}
+
+variable "my_ip" {
+  type        = string
+  description = "The IP address of the user"
+  default     = "73.97.223.46"
+}
+
+variable "key_name" {
+  type        = string
+  description = "The name of the key pair"
+}
+
+variable "listener_port" {
+  type        = number
+  description = "The port for the listener"
+  default     = 80
+}
+
+variable "webapp_port" {
+  type        = number
+  description = "The port for the webapp"
+  default     = 8081
+}
+
+variable "cooldown_period" {
+  type        = number
+  description = "The cooldown period for the autoscaling group"
+  default     = 60
+}
+
+variable "scaling_adjustment_up_number" {
+  type        = number
+  description = "The number of instances to add when scaling up"
+  default     = 1
+}
+
+variable "scaling_adjustment_down_number" {
+  type        = number
+  description = "The number of instances to remove when scaling down"
+  default     = -1
+}
+
+variable "health_check_grace_period" {
+  type        = number
+  description = "The grace period for the health check"
+  default     = 300
+}
+
+variable "tag_key" {
+  type        = string
+  description = "The key for the tag"
+  default     = "Application"
+}
+
+variable "tag_value" {
+  type        = string
+  description = "The value for the tag"
+  default     = "webapp"
+}
+
+variable "desired_capacity" {
+  type        = number
+  description = "The desired capacity for the autoscaling group"
+  default     = 1
+}
+
+variable "min_size" {
+  type        = number
+  description = "The minimum size for the autoscaling group"
+  default     = 1
+}
+
+variable "max_size" {
+  type        = number
+  description = "The maximum size for the autoscaling group"
+  default     = 3
+}
+
+variable "threshold_up" {
+  type        = number
+  description = "The threshold for scaling up"
+  default     = 5
+}
+
+variable "threshold_down" {
+  type        = number
+  description = "The threshold for scaling down"
+  default     = 3
+}
+
+variable "listener_protocol" {
+  type        = string
+  description = "The protocol for the listener"
+  default     = "HTTP"
+}
+
+variable "listener_action_type" {
+  type        = string
+  description = "The action type for the listener"
+  default     = "forward"
+}
+
+variable "interval" {
+  type        = number
+  description = "The interval for the health check"
+  default     = 30
+}
+
+variable "timeout" {
+  type        = number
+  description = "The timeout for the health check"
+  default     = 5
+}
+
+variable "healthy_threshold" {
+  type        = number
+  description = "The healthy threshold for the health check"
+  default     = 2
+}
+
+variable "unhealthy_threshold" {
+  type        = number
+  description = "The unhealthy threshold for the health check"
+  default     = 2
+}
+
+variable "scale_evaluation_periods" {
+  type        = number
+  description = "The evaluation periods for the scaling"
+  default     = 2
+}
+
+variable "scale_period" {
+  type        = number
+  description = "The period for the scaling"
+  default     = 120
+}
